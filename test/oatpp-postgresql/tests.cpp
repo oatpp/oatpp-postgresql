b109c462a7a81d9ae34b6de30853191075e12e0b
lganzzzo <lganzzzo@gmail.com>
2021-02-10 03:40:59 +0200

    Tests: Wait for DB to start.

--- test/oatpp-postgresql/tests.cpp
+++ test/oatpp-postgresql/tests.cpp
@@ -6,11 +6,33 @@
 #include "types/FloatTest.hpp"
 #include "types/InterpretationTest.hpp"
 
+
+#include "oatpp-postgresql/orm.hpp"
 #include "oatpp/core/base/Environment.hpp"
+
+#include <thread>
+#include <chrono>
 
 namespace {
 
 void runTests() {
+
+  OATPP_LOGI("Tests", "DB-URL='%s'", TEST_DB_URL);
+  auto connectionProvider = std::make_shared<oatpp::postgresql::ConnectionProvider>(TEST_DB_URL);
+  for(v_int32 i = 0; i < 6; i ++) {
+    try {
+      auto connection = connectionProvider->get();
+      if(connection) {
+        OATPP_LOGD("Tests", "Database is up! We've got a connection!");
+        break;
+      }
+    } catch (...) {
+      // DO NOTHING
+    }
+
+    OATPP_LOGD("Tests", "Database is not ready. Sleep 10s...");
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+  }
 
   OATPP_RUN_TEST(oatpp::test::postgresql::ql_template::ParserTest);
 
